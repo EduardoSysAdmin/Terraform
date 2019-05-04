f9bd9dc99c2dd2aba842e831d0715ee725d8ccf7
EduardoSysAdmin <45874069+EduardoSysAdmin@users.noreply.github.com>
2019-05-04 18:23:16 +0200

    Create vars.tf

---
+++ Openstack/Variables/vars.tf
@@ -0,0 +1,33 @@
+variable "nombre" {
+  default = "Variables"
+}
+variable "sabor" {
+  default = "1"
+}
+variable "imagen" {
+  default = "87bd9c2e-3f2e-4ed4-b9aa-bccf347acd8e"
+}
+variable "claves" {
+  default = "EOG"
+}
+variable "red-id" {
+  default = "2b9161ce-3dce-46f6-a242-ede7e1e8f9ff"
+}
+variable "red" {
+  default = "Privada1"
+}
+variable "usuario" {
+  default = "admin"
+}
+variable "tenant" {
+  default = "admin"
+}
+variable "openstack" {
+  default = "http://openstack:5000/"
+}
+variable "password" {
+  default = "bolson"
+}
+variable "red-publica" {
+  default = "Publica"
+}
